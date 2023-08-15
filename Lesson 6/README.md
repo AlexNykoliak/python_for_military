2ad4ef32592aeaa8b97ee8d643dba0f9d1cf9ac5
AlexNykoliak <nykolyak.o@gmail.com>
2023-08-15 22:17:06 +0300

    Add lesson 6

---
+++ Lesson 6/README.md
@@ -0,0 +1,5 @@
+1. Переглянутми відео з заняття
+2. Самостійно створюємо проект на Django
+3. Потренуйтесь з умовними операторами на цьому сайті:
+   https://www.bhutanpythoncoders.com/practice-questions-for-conditional-statements/
+
---
+++ Lesson 6/main.py
@@ -0,0 +1,136 @@
+country_dictionary = {
+#     "Ukraine": "Kyiv",
+#     "UK": "London",
+#     "Portugal": "Lisbon",
+# }
+
+
+# our_input = str(input("Enter city: "))
+# for country, city in country_dictionary.items():
+#     if our_input.lower() == city.lower():
+#         print(f"{country} 's capital is {city}")
+
+
+# for country in country_dictionary.keys():
+#     print(country)
+
+
+# for city in country_dictionary.values():
+#     print(city)
+
+
+
+# a = 10
+# b = 100
+
+
+# print("my number is", a)
+# print("my number is {}".format(a))
+
+# print(f"my number is {a}")
+
+
+# a = 1000
+# b = 1000
+
+# # print(a == b)
+# # print(a > b)
+# # print(a < b)
+
+# if a == b:
+#     print("ALL IS OK")
+# # elif a > b:
+# #     print("a is bigger than b, FAILED")
+# # elif a < b:
+# #     print("a is less than b, FAILED")
+
+# else:
+#     print("FAILED")
+
+
+# f = "hEllO23456789"
+
+
+# for i in f:
+#     if i.isupper():
+#         print(f'у нашому слові є велика буква, і ця буква - {i}')
+
+
+# if type(f) == str:
+#     print("YES")
+
+
+# a = 2
+# b = 330
+
+# print("A") if a > b else print("B")
+
+# if a > b:
+#     print("A")
+# else:
+#     print("B")
+
+
+# a = 331
+# b = 332
+# print("A") if a > b else print("=") if a == b else print("B")
+
+
+# a = 10
+# b = 20.0
+
+# if type(a) == int and type(b) == int:
+#     print("YES, both are int")
+# else:
+#     print(f"NO, one of them is not int, a is {type(a)}")
+
+# a = 10
+# b = 20.0
+
+# if type(a) == int or type(b) == int:
+#     print("YES")
+# else:
+#     print(f"NO, one of them is not int, a is {type(a)}")
+
+# country_list = {'aouei'}
+# b = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# for i in alphabet:
+#     if i in country_list:
+#         print(f"YES, we have {i}")
+#     else:
+#         print(f"NO, we dont have {i}")
+
+
+# x = 123
+# if x > 10:
+#     print("Above ten,")
+
+#     if x > 20:
+#         print("and also above 20!")
+#     else:
+#         print("but not above 20.")
+
+
+# else:
+#     print("Below ten!")
+
+# import random
+# numbers = [random.randint(100, 110) for i in range(100)] #
+
+# # for i in range(100):
+# #     print(random.randint(100, 110),end="\n")
+
+
+# for i in numbers:
+#     if i == 5:
+#         pass # go out of the loop
+#     print(i)
+
+
+# continue # go to next iteration
+# break #go out of the loop
+# pass
